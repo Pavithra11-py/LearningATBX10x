54a9845ebd5f585b7ad3de013ad31a12e035dbb8
pavithra <pavithranaya1108@gmail.com>
2025-01-11 11:49:22 +0530

    day3

---
+++ src/Lab01comments.java
@@ -0,0 +1,13 @@
+public class Lab01comments {
+    public static void main(String[] args) {
+        //this is single line comment
+//      using ctrl slash   this
+//                is a
+//                        multiline comment
+        /* multi
+        lines
+        comments
+         */
+        System.out.println("Hello world everone");
+    }
+}
---
+++ src/table_9.java
@@ -0,0 +1,25 @@
+public class variables {
+    public static void main(String[] args) {
+        int num1 = 9,num2=18,num3=27,num4=36,num5=45,num6=54,num7=63,num8=72,num9=81,num10=90;
+
+        System.out.printf("%d*1=%d",num1,num1);
+        System.out.println();
+        System.out.printf("%d*2=%d",num1,num2);
+        System.out.println();
+        System.out.printf("%d*3=%d",num1,num3);
+        System.out.println();
+        System.out.printf("%d*4=%d",num1,num4);
+        System.out.println();
+        System.out.printf("%d*5=%d",num1,num5);
+        System.out.println();
+        System.out.printf("%d*6=%d",num1,num6);
+        System.out.println();
+        System.out.printf("%d*7=%d",num1,num7);
+        System.out.println();
+        System.out.printf("%d*8=%d",num1,num8);
+        System.out.println();
+        System.out.printf("%d*9=%d",num1,num9);
+        System.out.println();
+        System.out.printf("%d*10=%d",num1,num10);
+    }
+}
