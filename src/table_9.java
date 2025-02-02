c1d3f5b947248d3363d787903ccd8fd41b6d205c
Pavithra panduga <pavithranaya1108@gmail.com>
2025-02-02 13:50:57 +0530

    Update table_9.java

--- src/table_9.java
+++ src/table_9.java
@@ -1,9 +1,22 @@
 public class table_9 {
     public static void main(String[] args) {
         int num = 9;
-        for(int i = 1; i <= 10; ++i)
-        {
-            System.out.printf("%d * %d = %d \n", num, i, num * i);
-        }
+    //    for(int i = 1; i <= 10; ++i)
+    //    {
+    //        System.out.printf("%d * %d = %d \n", num, i, num * i);
+    //    }
+     
+        System.out.printf("%d X %d = %d\n",num,1, num*1);
+        System.out.printf("%d X %d = %d\n",num,2, num*2);
+        System.out.printf("%d X %d = %d\n",num,3, num*3);
+        System.out.printf("%d X %d = %d\n",num,4, num*4);
+        System.out.printf("%d X %d = %d\n",num,5, num*5);
+        System.out.printf("%d X %d = %d\n",num,6, num*6);
+        System.out.printf("%d X %d = %d\n",num,7, num*7);
+        System.out.printf("%d X %d = %d\n",num,8, num*8);
+        System.out.printf("%d X %d = %d\n",num,9, num*9);
+        System.out.printf("%d X %d = %d\n",num,10, num*10);
+
+
     }
 }
